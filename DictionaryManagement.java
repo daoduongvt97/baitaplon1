bda7e608a6acc4bc92e712c73647f9045db90160
doanphuongthao <35805414+doanphuongthao@users.noreply.github.com>
2018-10-22 23:27:05 +0700

    BTL

---
+++ DictionaryManagement.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary_btl;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class DictionaryManagement{
+    Dictionary tuDien = new Dictionary();
+    private String dirFileDic; //đường dẫn tới file dữ liệu từ điển
+    //constructor khởi tạo đường dẫn tới file dữ liệu của từ điển tương ứng
+    public DictionaryManagement(String dir) {
+        super();
+        switch (dir) {
+            case "ev":
+                this.dirFileDic="file/EV.txt";
+                break;
+            case "ve":
+                this.dirFileDic="file/VE.txt";
+                break;
+            default:
+                break;
+        }
+    }
+    //hàm tìm từ word
+    public Word lookUp(String word){
+        Word tmp = null;
+        if (tuDien.tuVung.size() == 0) return null;
+        else {
+            for(int i = 0;i < tuDien.getSize(); i++){
+                if(tuDien.getWord(i).getWord_target().equals(word))
+                tmp = tuDien.getWord(i);
+            }
+            return tmp;
+        }
+    }
+    public String deleteWord(String word){
+        if(this.lookUp(word)!=null) {tuDien.tuVung.remove(this.lookUp(word));
+            return "Đã xóa!";
+        }
+        else return "Không thấy!";
+    }
+    public String addWord(String word_target, String word_explain, String type){
+        Word tmp = this.lookUp(word_target);
+        if(tmp != null){
+            tmp.setType(type);
+            tmp.setWord_target(word_target);
+            tmp.setWord_explain(word_explain);
+            return "Đã sửa!";
+        }
+        else {
+            tuDien.addWord(word_target, word_explain, type);
+            return "Đã thêm!";
+        }
+    }
+    public ArrayList<Word> seacher(String tuKhoa){
+        ArrayList<Word> ketQua = new ArrayList<>();
+
+        for (int i = 0; i <tuDien.getSize();i ++){
+            if(tuDien.getWord(i).getWord_target().startsWith(tuKhoa)){
+                ketQua.add(tuDien.getWord(i));
+            }
+        }
+        return ketQua;
+    }
+    public void insertFromFile() {
+        try{
+            File fileDir = new File(dirFileDic); //đọc file có đường dẫn là dirFileDic
+            try (BufferedReader sc = new BufferedReader(new InputStreamReader(new FileInputStream(fileDir), "UTF-8"))) {
+                String line = null;
+                while ((line = sc.readLine()) != null){ //đọc vào từng dòng đến cuối file
+                    String result[] = line.split(":");//tách các chuỗi cách nhau bởi dấu : trong đó chuỗi đầu là từ, chuối thứ hai là nghĩa, chuỗi thứ 3 là từ loại
+                    tuDien.addWord(result[0].trim(),result[1].trim(), result[2].trim()); //thêm vào mảng động
+                }
+            }
+        }
+        catch (IOException e){
+        }
+
+    }
+    public void dictionaryExportToFile(){
+        tuDien.sort();// sắp xếp lại danh sach theo alphabe
+        try {
+            FileOutputStream f= new FileOutputStream(dirFileDic,false);
+            try (PrintWriter wr = new PrintWriter(f)) {
+                for(int i = 0; i < tuDien.getSize();i++){ //duyệt từ đầu tới cuối danh sách và ghi dữ liệu vào file
+                    wr.println(tuDien.getWord(i).getWord_target().toLowerCase()+ " : " + 
+                            tuDien.getWord(i).getWord_explain().toLowerCase()+ " : " 
+                            + tuDien.getWord(i).getType()); 
+                }//khi lưu file chuyển về dạng chữ in thường
+            }
+        }
+        catch (IOException e){
+        }
+    }
+}
---
+++ Word.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary_btl;
+
+import java.util.Comparator;
+
+public class Word {
+    private String word_target;
+    private String word_explain;
+    private String type;
+    
+    public Word(){
+        word_target = "";
+        word_explain = "";
+    }
+    public Word(String word_target, String word_explain, String type){
+        this.word_explain = word_explain;
+        this.word_target = word_target;
+        this.type = type;
+    }
+    //ham getter setter
+    public String getWord_target() {
+        return word_target;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    public String getWord_explain() {
+        return word_explain;
+    }
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" + "word_target=" + word_target + ", word_explain=" + word_explain + ", type=" + type + '}';
+    }
+}
+
+class soSanh implements Comparator<Word>{
+    @Override
+    public int compare(Word o1, Word o2) {
+        return o1.getWord_target().compareTo(o2.getWord_target());
+    }
+}
